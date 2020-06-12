a603a088c6e63fb4db25ce78cbbb14e18ca5ea3f
Vincent Delaleu <dev@vincentdelaleu.com>
2020-06-12 11:57:30 +0200

    Updates

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "/delvvince.github.io",
-  "titleTemplate": "%s - DPT",
-  "siteUrl": "https://delvvince.github.io",
-  "version": "0.7.17",
-  "catchLinks": true
-}
